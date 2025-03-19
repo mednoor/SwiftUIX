b4cd4e4d688204c889c6b8d6ce69f9894e65b2d9
Mohamed Aberkane <213eek@gmail.com>
2025-03-19 21:49:21 +0400

    fixed issues related to concurrency

--- Sources/Intramodular/Miscellaneous/ActionLabelView.swift
+++ Sources/Intramodular/Miscellaneous/ActionLabelView.swift
@@ -5,32 +5,45 @@
 import Swift
 import SwiftUI
 
+import Swift
+import SwiftUI
+
 public protocol ActionLabelView: View {
     associatedtype Label: View
-    
-    init(action: @escaping () -> Void, @ViewBuilder label: () -> Label)
+
+    @MainActor
+    init(action: @escaping @MainActor () -> Void, @ViewBuilder label: () -> Label)
+    @MainActor
     init(action: Action, @ViewBuilder label: () -> Label)
 }
 
 // MARK: - Implementation-
 
 extension ActionLabelView {
-    public init(action: @escaping () -> Void, @ViewBuilder label: () -> Label) {
-        self.init(action: .init(action), label: label)
+    @_disfavoredOverload
+    @MainActor
+    public init(
+        action: @escaping @MainActor () -> Void,
+        @ViewBuilder label: () -> Label
+    ) {
+        self.init(action: Action(action), label: label)
     }
 }
 
 // MARK: - Extensions
 
 extension ActionLabelView {
+    @MainActor
     public init(action: Action, label: Label) {
         self.init(action: action, label: { label })
     }
     
-    public init(action: @escaping () -> Void, label: Label) {
+    @MainActor
+    public init(action: @escaping @MainActor () -> Void, label: Label) {
         self.init(action: .init(action), label: { label })
     }
     
+    @MainActor
     public init(
         dismiss presentation: Binding<PresentationMode>,
         @ViewBuilder label: () -> Label
@@ -38,6 +51,7 @@ extension ActionLabelView {
         self.init(action: { presentation.wrappedValue.dismiss() }, label: label)
     }
     
+    @MainActor
     public init(
         dismiss presentation: PresentationManager,
         @ViewBuilder label: () -> Label
@@ -45,6 +59,7 @@ extension ActionLabelView {
         self.init(action: { presentation.dismiss() }, label: label)
     }
     
+    @MainActor
     public init(
         toggle boolean: Binding<Bool>,
         @ViewBuilder label: () -> Label
@@ -52,6 +67,7 @@ extension ActionLabelView {
         self.init(action: { boolean.wrappedValue.toggle() }, label: label)
     }
     
+    @MainActor
     @available(iOS 13.0, tvOS 13.0, *)
     @available(macOS, unavailable)
     @available(watchOS, unavailable)
@@ -64,6 +80,7 @@ extension ActionLabelView {
 }
 
 extension ActionLabelView where Label == Image {
+    @MainActor
     public init(
         systemImage: SFSymbolName,
         action: @escaping () -> Void
@@ -76,6 +93,7 @@ extension ActionLabelView where Label == Image {
 
 @available(iOS 14.0, OSX 10.16, tvOS 14.0, watchOS 7.0, *)
 extension ActionLabelView where Label == Text {
+    @MainActor
     public init<S: StringProtocol>(
         _ title: S,
         toggle boolean: Binding<Bool>
@@ -83,6 +101,7 @@ extension ActionLabelView where Label == Text {
         self.init(title, action: { boolean.wrappedValue.toggle() })
     }
     
+    @MainActor
     public init<S: StringProtocol>(
         _ title: S,
         action: @escaping () -> Void
@@ -93,22 +112,9 @@ extension ActionLabelView where Label == Text {
     }
 }
 
-// FIXME: Uncomment once Xcode 13.3 fixes this segfault.
-/*@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
-extension Button where Label == SwiftUI.Label<Text, Image> {
-    public init(
-        _ title: String,
-        systemImage: SFSymbolName,
-        action: @escaping () -> Void
-    ) {
-        self.init(action: action) {
-            Label(title, systemImage: systemImage)
-        }
-    }
-}*/
-
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 extension Button where Label == SwiftUI.Label<Text, Image> {
+    @MainActor
     public init(
         _ title: String,
         systemImage: SFSymbolName,
@@ -120,10 +126,29 @@ extension Button where Label == SwiftUI.Label<Text, Image> {
     }
 }
 
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+extension Button {
+    @MainActor
+    public init<Icon: View>(
+        _ title: String,
+        action: @escaping () -> Void,
+        @ViewBuilder icon: () -> Icon
+    ) where Label == SwiftUI.Label<Text, Icon> {
+        self.init(action: action) {
+            Label(title, icon: icon)
+        }
+    }
+}
+
 // MARK: - Conformances
 
 extension Button: ActionLabelView {
-    public init(action: Action, @ViewBuilder label: () -> Label) {
-        self.init(action: action.perform, label: label)
+    @_disfavoredOverload
+    @MainActor
+    public init(
+        action: Action,
+        @ViewBuilder label: () -> Label
+    ) {
+        self.init(action: { action.perform() }, label: label)
     }
 }
